d5bdb503bc24cc81cebcd4c5ca036f7aece7d0c2
bakemonostan <ehizomokhomion@gmail.com>
2022-10-04 19:46:42 -0700

    nav almost complete

---
+++ src/components/Navbar.vue
@@ -0,0 +1,17 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <header>
+    <nav
+      class="w-full flex justify-between items-center py-8 px-4 bg-white shadow-md"
+    >
+      <div><h1 class="font-bold">Where in the world</h1></div>
+      <div><p class="font-bold text-xs">Dark Mode</p></div>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="py-2">
     <h1 class="text-xl font-bold underline"> Hello world! </h1>
   </div>
 </template>
